baba4a1d74b02b4418ea04b28bb89d22621fdea1
Nisar K <nisardote@gmail.com>
2025-10-08 11:47:42 +0530

    Initial commit for server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// server.js
 import jsonServer from "json-server";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Fix for __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router(path.join(__dirname, "db.json")); // make sure db.json exists
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
